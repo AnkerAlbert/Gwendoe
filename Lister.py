25555dd55d04a367bc18fad1a7d5a5dfb9227f41
AnkerAlbert <anker.albert@avoconsulting.no>
2022-09-06 14:38:24 +0200

    fixed minor issues, added numpy

--- Lister.py
+++ Lister.py
@@ -1,6 +1,10 @@
 listA = []
 for i in range(15):
     listA.append(i+1)
-    list.apped("i")
+    list.append("i")
 
 print(listA)
+
+# From here I can start working with numpy
+
+import numpy as np
